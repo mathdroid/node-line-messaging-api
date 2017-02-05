8c1f43bdba83486103010443a31f776c53fe2e32
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2017-02-05 17:26:49 +0700

    change export method to make it ES5 compatible

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 import Bot from './bot'
 import Messages from './messages'
-//
-// let bot = new Bot('secret', 'token')
-// bot.on('webhook', w => console.log(`bot listens on port ${w}.`))
-// bot.on('message', m => console.log(m))
 
-// let arr = [{}]
-
-// console.log(Array.isArray(arr[0]))
-// console.log(Messages)
-// let msg = new Messages()
-// console.log(msg.addText('hi').addText('lol').addText({text: 'harambe'}).commit())
-export { Messages }
-export default Bot
+Bot.Messages = Messages
+module.exports = Bot
